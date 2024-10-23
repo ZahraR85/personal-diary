e85e09f7272416a0f4ae3f7d11bd16489abe2ce1
Silviya <silviyamaruthikkal@gmail.com>
2024-10-23 23:27:37 +0200

    Added  validation to AddEntryModal

---
+++ src/components/EntryDetailsModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EntryDetailsModal = () => {
+  return (
+    <div>
+      <h2>Entry Details</h2>
+      {/* Your modal content here */}
+    </div>
+  );
+};
+
+export default EntryDetailsModal;  // 
